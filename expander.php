6ea284ee5b9ca2b556f187936d62e40a048b924b
butterflymedia <butterflymedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-25 19:26:38 +0000

    = 0.2 = * FEATURE: Added "Read more"/"Read less" feature * IMPROVEMENT: New shortcode structure (see plugin description) * IMPROVEMENT: Removed filters * PERFORMANCE: Moved shortcode creation to plugin init() * PERFORMANCE: Merged all Javascript code
    git-svn-id: https://plugins.svn.wordpress.org/expander/trunk@810340 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- expander.php
+++ expander.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Expander
-Version: 0.1.1.2
+Version: 0.2
 Plugin URI: http://getbutterfly.com/wordpress-plugins/wordpress-expander/
 Description: Text expander plugin. Just like popcorn. Click and pop.
 Author: Ciprian Popescu
@@ -20,66 +20,49 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
- * Usage: [wpex Read more]hidden text[/wpex]
+ * Usage: [wpex more="Read more" less="Read less"]hidden text[/wpex]
  */
 
 define('WPEX_PLUGIN_URL', WP_PLUGIN_URL . '/' . dirname(plugin_basename(__FILE__)));
 define('WPEX_PLUGIN_PATH', WP_PLUGIN_DIR . '/' . dirname(plugin_basename(__FILE__)));
+define('WPEX_VERSION', '0.2');
 
-function wpex_str_replace_once($needle, $replace, $haystack) {
-	$pos = strpos($haystack, $needle);
-	if($pos === false)
-		return $haystack;
-
-	return substr_replace($haystack, $replace, $pos, strlen($needle));
+function wpex_register_shortcodes() {
+   add_shortcode('wpex', 'wpex_main');
 }
 
-function wpex_process($content) {
-	$offset = 0;
-	$stag = '[wpex ';
-	$etag = '[/wpex]';
-	while(stripos($content, $stag, $offset)) {
-		// string to replace
-		$s = stripos($content, $stag, $offset);	
-		$e = stripos($content, $etag, $s) + strlen($etag); 
+function wpex_main($atts, $content = null) {
+	extract(shortcode_atts(array(
+		'more' => 'Read more',
+		'less' => 'Read less'
+	), $atts));
 
-		// inside data
-		$ds = stripos($content, ']', $s) + 1;
-		$de = $e - strlen($etag);
+	mt_srand((double)microtime() * 1000000);
+	$rnum = mt_rand();
 
-		// style tag
-		$ss = $s + strlen($stag);
-		$se = $ds - 1;
+	$new_string = '<a onclick="wpex_toggle(' . $rnum . ', \'' . $more . '\', \'' . $less . '\'); return false;" class="wpex-link" id="wpexlink' . $rnum . '" href="#">' . $more . '</a>' . "\n";
+	$new_string .= '<p class="wpex_div" id="wpex' . $rnum . '" style="display: none;">' . $content . '</p>';
 
-		$sstring = substr($content, $s, $e - $s);
-		$sdesc = substr($content, $ss, $se - $ss); 
-		$sdata = substr($content, $ds, $de - $ds);
-
-		mt_srand((double)microtime() * 1000000);
-		$rnum = mt_rand();
-
-		$new_string = '<a class="wpex-link" style="display: none;" id="wpexlink' . $rnum . '" href="javascript:expand(document.getElementById(\'wpex' . $rnum . '\'))">' . $sdesc . '</a>' . "\n";
-		$new_string .= '<div class="wpex_div" id="wpex' . $rnum . '">';
-		$new_string .= '<script>expand(document.getElementById(\'wpex' . $rnum . '\')); expand(document.getElementById(\'wpexlink' . $rnum . '\'))</script>';
-
-		$sdata = preg_replace('`^<br />`sim', '', $sdata);
-
-		$content = wpex_str_replace_once($sstring, $new_string . $sdata . '</div>', $content);
-
-		$offset = $s + 1;
-	}
-
-	return $content;
+	return wpautop($new_string);
 }
 
 function wpex_javascript() {
-	echo '<script>function expand(param) { param.style.display = (param.style.display == "none") ? "" : "none"; }</script>';
+	echo '<script>
+	function expand(param) {
+		param.style.display = (param.style.display == "none") ? "block" : "none";
+	}
+	function wpex_toggle(id, more, less) {
+		el = document.getElementById("wpexlink" + id);
+		el.innerHTML = (el.innerHTML == more) ? less : more;
+		expand(document.getElementById("wpex" + id));
+	}
+	</script>';
 }
 
 add_action('wp_head', 'wpex_javascript');
-add_filter('the_content', 'wpex_process');
+add_action('init', 'wpex_register_shortcodes');
 ?>
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: butterflymedia, getbutterfly
 Tags: javascript, expand, expander, toggle, show, hide
 License: GPLv3
-Requires at least: 3.0
+Requires at least: 3.5
 Tested up to: 3.8-beta-1
-Stable tag: 0.1.1.2
+Stable tag: 0.2
 
 == Description ==
 
@@ -12,17 +12,25 @@ Text expander plugin. Just like popcorn. Click and pop.
 
 Use it to toggle (show/hide) blocks of text, by inserting a simple shortcode:
 
-`[wpex Read more]hidden text[/wpex]`
+`[wpex more="Read more" less="Read less"]hidden text[/wpex]`
 
-Replace "Read more" with your desired text (call to action).
+Replace "Read more" and "Read less" with your desired text. Use ".wpex-link" class to style the links.
 
 == Installation ==
 
 1. Upload the plugin folder to your `/wp-content/plugins/` directory
 2. Activate the plugin via the Plugins menu in WordPress
-3. Create and publish a new page and add this shortcode: `[wpex Read more]hidden text[/wpex]`
+3. Create and publish a new page and add this shortcode: `[wpex more="Read more" less="Read less"]hidden text[/wpex]`
+4. If you upgraded from 0.1.x change your `[wpex Read more]` to `[wpex more="Read more" less="Read less"]`
 
 == Changelog ==
+
+= 0.2 =
+* FEATURE: Added "Read more"/"Read less" feature
+* IMPROVEMENT: New shortcode structure (see plugin description)
+* IMPROVEMENT: Removed filters
+* PERFORMANCE: Moved shortcode creation to plugin init()
+* PERFORMANCE: Merged all Javascript code
 
 = 0.1.1.2 =
 * Checked WordPress 3.8-beta compatibility
